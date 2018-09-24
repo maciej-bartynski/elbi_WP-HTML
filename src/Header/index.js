5cc61c635dfda211f446731fa090f0aa35eb0376
JestemBartek <maciek.bartynski@gmail.com>
2018-09-24 22:49:31 +0200

    header init

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,37 @@
-import $ from "jquery";
-
 document.addEventListener('DOMContentLoaded', function(){
+    let imgs = document.querySelectorAll('.el1_image');
+    let wrapper = document.querySelector("#el1_slider-element")
+    let leng = imgs.length;
+    let positions = [];
+    for (let i=0;i<leng;i++){
+        positions.push(i*-100);
+    }
     
+    setTimeout(()=>{left(leng-2)},8000);
+    
+    function left (x){
+        slider(x);
+        x--;
+        if (x>0){
+            setTimeout(()=>{left(x)}, 8000)
+        } else {
+            setTimeout(()=>{right(x)}, 8000)
+        }
+    }
+
+    function right (x){
+        slider(x);
+        x++;
+        if (x < leng-1) {
+            setTimeout(()=>{right(x)}, 8000)
+        } else {
+            setTimeout(()=>{left(x)}, 8000)
+        }
+    }
+
+    function slider(x){
+        x=positions[x];
+        wrapper.style.left=x+'%';
+    }
+
 });
